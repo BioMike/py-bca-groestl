0f69eff7d77246c1dda0e342caab44ce0acacd9b
Myckel Habets <myckel@sdf.lonestar.org>
2017-01-27 23:23:52 +0100

    WIP: Groestl-myr hash support

--- groestlmodule.c
+++ groestlmodule.c
@@ -1,6 +1,28 @@
 #include <Python.h>
 
 #include "sph_groestl.h"
+#include "sph_sha2.h"
+
+void groestlmyr_hash(const char* input, int length, char* output)
+{
+    /* FIXME: length is ignored */
+
+    sph_groestl512_context ctx_groestl;
+    sph_sha256_context ctx_sha2;
+
+    uint32_t hash[16];
+
+    sph_groestl512_init(&ctx_groestl);
+    sph_groestl512(&ctx_groestl, input, 80);
+    sph_groestl512_close(&ctx_groestl, (void*) hash);
+
+    sph_sha256_init(&ctx_sha2);
+    sph_sha256(&ctx_sha2, hash, 64);
+    sph_sha256_close(&ctx_sha2, (void*) hash);
+
+    memcpy(output, hash, 32);
+}
+
 
 static void GroestlCoinHash(const char *input, int length, char *output)
 {
@@ -9,12 +31,12 @@ static void GroestlCoinHash(const char *input, int length, char *output)
     sph_groestl512_context ctx_groestl[2];
 
     sph_groestl512_init(&ctx_groestl[0]);
-    sph_groestl512 (&ctx_groestl[0], input, length); 
-    sph_groestl512_close(&ctx_groestl[0], hashA); 
+    sph_groestl512 (&ctx_groestl[0], input, length);
+    sph_groestl512_close(&ctx_groestl[0], hashA);
     
     sph_groestl512_init(&ctx_groestl[1]);
-    sph_groestl512 (&ctx_groestl[1], hashA, 64); 
-    sph_groestl512_close(&ctx_groestl[1], hashB); 
+    sph_groestl512 (&ctx_groestl[1], hashA, 64);
+    sph_groestl512_close(&ctx_groestl[1], hashB);
     
     memcpy(output, hashB, 32);
 }
@@ -49,27 +71,58 @@ static PyObject *groestlcoin_gethash(PyObject *self, PyObject *args)
     return value;
 }
 
-static PyMethodDef GroestlcoinMethods[] = {
-    { "getHash", groestlcoin_gethash, METH_VARARGS, "Returns the groestlcoin hash" },
+static PyObject *groestlmyr_gethash(PyObject *self, PyObject *args)
+{
+    char *output;
+    PyObject *value;
+#if PY_MAJOR_VERSION >= 3
+    PyBytesObject *input;
+#else
+    PyStringObject *input;
+#endif
+    int length;
+    if (!PyArg_ParseTuple(args, "Si", &input, &length))
+        return NULL;
+    Py_INCREF(input);
+    output = PyMem_Malloc(32);
+
+#if PY_MAJOR_VERSION >= 3
+    groestlmyr_hash((char *)PyBytes_AsString((PyObject*) input), length, output);
+#else
+    groestlmyr_hash((char *)PyString_AsString((PyObject*) input), length, output);
+#endif
+    Py_DECREF(input);
+#if PY_MAJOR_VERSION >= 3
+    value = Py_BuildValue("y#", output, 32);
+#else
+    value = Py_BuildValue("s#", output, 32);
+#endif
+    PyMem_Free(output);
+    return value;
+}
+
+static PyMethodDef GroestlMethods[] = {
+    { "getGroestlcoinHash", groestlcoin_gethash, METH_VARARGS, "Returns the groestlcoin hash" },
+    { "getGroestlMyrHash", groestlmyr_gethash, METH_VARARGS, "Returns the Myriadcoin Groestl hash" },
     { NULL, NULL, 0, NULL }
 };
 
 #if PY_MAJOR_VERSION >= 3
-static struct PyModuleDef GroestlcoinModule = {
+static struct PyModuleDef GroestlModule = {
     PyModuleDef_HEAD_INIT,
-    "groestlcoin_hash",
+    "groestl_hash",
     "...",
     -1,
-    GroestlcoinMethods
+    GroestlMethods
 };
 
-PyMODINIT_FUNC PyInit_groestlcoin_hash(void) {
-    return PyModule_Create(&GroestlcoinModule);
+PyMODINIT_FUNC PyInit_groestl_hash(void) {
+    return PyModule_Create(&GroestlModule);
 }
 
 #else
 
-PyMODINIT_FUNC initgroestlcoin_hash(void) {
-    (void) Py_InitModule("groestlcoin_hash", GroestlcoinMethods);
+PyMODINIT_FUNC initgroestl_hash(void) {
+    (void) Py_InitModule("groestl_hash", GroestlMethods);
 }
 #endif
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-groestl_hash_module = Extension('groestl_hash', sources = ['groestlmodule.c', 'groestl.c'])
+groestl_hash_module = Extension('groestl_hash', sources = ['groestlmodule.c', 'groestl.c', 'sha2.c'])
 
 setup (name = 'groestl_hash',
        version = '1.0',
