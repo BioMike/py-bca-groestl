4df33db9a414dc02a6a4c751d64695e7baa01374
Myckel Habets <myckel@sdf.lonestar.org>
2017-11-06 22:41:16 +0100

    Push new version

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup, Extension
 groestl_hash_module = Extension('groestl_hash', sources = ['groestlmodule.c', 'groestl.c', 'sha2.c'])
 
 setup (name = 'groestl_hash',
-       version = '1.0',
+       version = '1.1',
        ext_modules = [groestl_hash_module])
